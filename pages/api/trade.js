e50fece13ed4ba1d67fb7c8821f7da2aaa3a6c90
btbjoe <141979971+btbebunt@users.noreply.github.com>
2025-04-22 17:17:10 +0800

    asd

--- pages/api/trade.js
+++ pages/api/trade.js
@@ -222,7 +222,7 @@ export default async (req, res) => {
           chat_id: TELEGRAM_CHAT_ID,
           text: message,
           parse_mode: 'Markdown',
-          reply_to_message_id: replyMessageId,
+          reply_to_message_id: Number(replyMessageId),
         },
         { timeout: 5000 }
       );
